d3b336314c600cc819dfb69f78680b55ccdfe5d9
Kevin Rees <krees22@gmail.com>
2021-12-06 21:16:21 -0700

    Some style test stuff

--- app_remote/menus.c
+++ app_remote/menus.c
@@ -31,18 +31,25 @@ static void main_menu_init( void )
 	if ( menus.main_menu == NULL ) {
 		menus.main_menu = lv_obj_create(NULL);
 
+		static lv_obj_t * bg;
+		bg = lv_obj_create(menus.main_menu);
+		lv_obj_set_size( bg, BTN_WIDTH, BTN_HEIGHT );
+
 		static lv_style_t style;
 		lv_style_init(&style);
-		lv_style_set_border_width(&style, 2);
+		lv_style_set_border_width(&style, 1);
 		//lv_style_set_outline_color(&style, lv_palette_main(LV_PALETTE_BLUE));
 		//lv_style_set_outline_pad(&style, 8);
 
-		lv_obj_add_style(menus.main_menu, &style, 0);
+		//lv_obj_add_style(menus.main_menu, &style, 0);
+		//lv_obj_add_style(bg, &style, 0);
 
 		static lv_obj_t * btn1;
 		btn1 = lv_btn_create( menus.main_menu );
+		//btn1 = lv_btn_create( bg );
 		lv_obj_add_event_cb( btn1, button_handler, LV_EVENT_ALL, NULL );
 		lv_obj_set_size( btn1, BTN_WIDTH, BTN_HEIGHT );
+		//lv_obj_set_size( btn1, LV_SIZE_CONTENT, LV_SIZE_CONTENT );
 		lv_obj_align( btn1, LV_ALIGN_LEFT_MID, 2, 0 );
 
 		static lv_obj_t * lbl1;
@@ -52,14 +59,19 @@ static void main_menu_init( void )
 
 		static lv_obj_t * btn2;
 		btn2 = lv_btn_create( menus.main_menu );
+		//btn2 = lv_btn_create( bg );
 		lv_obj_add_event_cb( btn2, button_handler, LV_EVENT_ALL, NULL );
 		lv_obj_set_size( btn2, BTN_WIDTH, BTN_HEIGHT );
+		//lv_obj_set_size( btn2, LV_SIZE_CONTENT, LV_SIZE_CONTENT );
 		lv_obj_align_to( btn2, btn1, LV_ALIGN_OUT_RIGHT_MID, 3, 0 );
 
 		static lv_obj_t * lbl2;
 		lbl2 = lv_label_create(btn2);
 		lv_label_set_text( lbl2, "Test2" );
 		lv_obj_center(lbl2);
+
+		//lv_obj_add_style(btn2, &style, 0);
+
 	}
 }
 
